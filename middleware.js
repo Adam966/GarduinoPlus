8904e71dcad301c12ee039c80ed22359d52f3b8b
lenartd <daniel.lenart@akademiasovy.sk>
2019-06-06 10:59:31 +0200

    token verification error handling

--- middleware.js
+++ middleware.js
@@ -2,28 +2,40 @@ let jwt = require('jsonwebtoken');
 const config = require('./config.js');
 
 let checkToken = (req, res, next) => {
-  let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length);
-  }
+  try
+  {
+      let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+      if (token.startsWith('Bearer ')) 
+      {
+        token = token.slice(7, token.length);
+      }
 
-  if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
-      if (err) {
+      if (token) 
+      {
+        jwt.verify(token, config.secret, (err, decoded) => {
+          if (err) {
+            return res.json({
+              success: false,
+              message: 'Token is not valid'
+            });
+          } else {
+            req.decoded = decoded;
+            next();
+          }
+        });
+      } 
+      else 
+      {
         return res.json({
           success: false,
-          message: 'Token is not valid'
+          message: 'Auth token is not supplied'
         });
-      } else {
-        req.decoded = decoded;
-        next();
       }
-    });
-  } else {
-    return res.json({
-      success: false,
-      message: 'Auth token is not supplied'
-    });
+  }
+  catch(err)
+  {
+    console.log("ERROR IN VERIFICATION");
+    console.log(err);
   }
 };
 
